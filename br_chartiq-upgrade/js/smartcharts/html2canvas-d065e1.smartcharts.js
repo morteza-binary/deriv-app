a2b54c95216be0eb0908d98128450087ebe835d9
Morteza <morteza@binary.com>
2019-11-11 17:06:17 +0800

    Updates

---
+++ br_chartiq-upgrade/js/smartcharts/html2canvas-d065e1.smartcharts.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpsmartcharts"] = window["webpackJsonpsmartcharts"] || []).push([["html2canvas"],{
+
+/***/ "./chartiq/html2canvas.min.js":
+/*!************************************!*\
+  !*** ./chartiq/html2canvas.min.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/eslint-loader/index.js):\nError: ENOENT: no such file or directory, open '/Users/Morteza/Projects/Binary/SmartCharts/chartiq/html2canvas.min.js'");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=html2canvas-d065e1.smartcharts.js.map
